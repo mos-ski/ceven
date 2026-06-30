ff39aede74ef8a5ff7c3d2372b1fad2fac0a2a8c
Moski <adedamolamoses@gmail.com>
2026-06-30 23:02:30 +0100

    feat(caregiver): add /parent alias route

---
+++ app/(caregiver)/parent/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+export default function ParentRoot() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace("/caregiver/home");
+  }, [router]);
+
+  return null;
+}
