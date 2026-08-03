2a067be021dd1ac2c33a9094ec068922346d42c6
Moski <adedamolamoses@gmail.com>
2026-08-04 00:39:22 +0100

    fix: revert to clean state, only flex-1 min-h-0 on runtime for proper phone frame sizing

--- components/parent-caregiver-v3/source-artifact-runtime.tsx
+++ components/parent-caregiver-v3/source-artifact-runtime.tsx
@@ -291,9 +291,9 @@ export function SourceArtifactRuntime({ markup, logic, styles, initialState }: R
   }, [Runtime, artifactState, markup, revision]);
 
   return (
-    <main className="flex flex-col flex-1 min-h-0 bg-[#EFE6D8]" style={{ lineHeight: "normal" }}>
+    <main className="flex-1 min-h-0 bg-[#EFE6D8]" style={{ lineHeight: "normal" }}>
       <style dangerouslySetInnerHTML={{ __html: styles }} />
-      <div ref={containerRef} className="flex flex-col flex-1 min-h-0" />
+      <div ref={containerRef} className="flex-1 min-h-0" />
     </main>
   );
 }
